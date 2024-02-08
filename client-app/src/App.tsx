42a7c122ead295a935856bcfbf1820743d7b05f6
Aavash <Dahal.aavash2520@gmail.com>
2024-02-08 16:01:36 +0530

    latest

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
 function App() {
-  return <h1>Reactivities</h1>;
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
+      setActivities(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header as="h2" icon="users" content="Reactivities" />
+
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
 }
 
 export default App;
